b354ee405a5c3904743dc35311a86df85a7741b8
BarisRahimi123 <BarisRahimi123@users.noreply.github.com>
2026-01-12 12:55:34 +0800

    Update Bundle ID to com.plansrow.billdora

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.primeledger.billdora',
+  appId: 'com.plansrow.billdora',
   appName: 'BillDora',
   webDir: 'dist'
 };
